4371bd951d8abe5f0fb7caccaf2e33888e4c2e74
mariamiah <mariah.kakande@gmail.com>
2020-02-20 18:21:40 +0300

    setup nginx reverse proxy

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,21 +1,28 @@
-#!/bin/bash
-mkdir -p ~/spotlights
-cd ~/spotlights || exit
+#!/usr/bin/env bash
+## gets run on host server
+# Instantly exists our script whenever an error occurs
+set -e
 
-rm -rf build.zip build
-
-wget https://github.com/devinit/data-hub-spotlights/releases/download/$1/build.zip
-
-echo 'finished downloading tag: $tag'
-
-unzip build.zip
-
+# Where to deploy our site on our server
+cd ../build.zip
+unzip -o build.zip
 cd build || exit
 
-docker build -t di-spotlights .
+# Build the docker image
+if [ ! "$(docker ps -q -f name=datahub-spotlights)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=datahub-spotlights)" ]; then
+        # cleanup
+        docker stop datahub-spotlights
+        docker rm datahub-spotlights
+        docker rmi datahub-spotlights-image
+    fi
+    # run container
+    docker-compose build
+    docker-compose up -d
+fi
+#docker run -d -p 3000:3000 --name datahub-spotlights -e ASSETS_SOURCE_URL="$ASSETS_SOURCE_URL" datahub-spotlights-image:latest
+# clean up scripts
 
-docker stop di-spotlights-latest
 
-docker rm di-spotlights-latest
-
-docker run -it -d -p 80:3000 --name di-spotlights-latest -e CMS_URL="$2" di-spotlights
+cd ~
+rm -rf deploy_spotlights
