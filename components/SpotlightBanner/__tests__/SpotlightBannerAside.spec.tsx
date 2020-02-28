e109d7fd3815e09b6d1a90f038dab48f0f6eae0c
David Ebukali <debukali@gmail.com>
2020-02-28 10:50:49 +0300

    Update snapshots for Spotlight Banner and Aside components

--- components/SpotlightBanner/__tests__/SpotlightBannerAside.spec.tsx
+++ components/SpotlightBanner/__tests__/SpotlightBannerAside.spec.tsx
@@ -8,7 +8,7 @@ import { SpotlightBannerAside } from '../SpotlightBannerAside';
 
 describe('SpotlightBannerAside', () => {
   test('renders correctly', () => {
-    const renderer = TestRenderer.create(<SpotlightBannerAside />).toJSON();
+    const renderer = TestRenderer.create(<SpotlightBannerAside classname={'spotlight__comparison'} />).toJSON();
 
     expect(renderer).toMatchSnapshot();
   });
