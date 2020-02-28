b81b8865a7d0d7dc2cf52aa7782b42bd85d82338
David Ebukali <debukali@gmail.com>
2020-02-28 17:28:43 +0300

    Add default field value to the chooseTheme prop in FormFieldSelect component

--- components/FormFieldSelect/FormFieldSelect.tsx
+++ components/FormFieldSelect/FormFieldSelect.tsx
@@ -8,16 +8,20 @@ interface FormFieldSelectItem {
 }
 
 interface FormFieldSelectProps extends SelectProps {
-  label: string;
+  label?: string;
   options: FormFieldSelectItem[];
+  chooseTheme?: 'light' | 'dark';
 }
 
-const FormFieldSelect: FunctionComponent<FormFieldSelectProps> = ({ label, options }) => {
+const FormFieldSelect: FunctionComponent<FormFieldSelectProps> = ({ label, options, chooseTheme: theme }) => {
   return (
     <>
       {label ? <label className="form-label">{label}</label> : null}
-      <Select options={options} />
+      <Select options={options} chooseTheme={theme} />
     </>
   );
 };
+
+FormFieldSelect.defaultProps = { chooseTheme: 'light' };
+
 export { FormFieldSelect };
