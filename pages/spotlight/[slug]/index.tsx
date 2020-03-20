706a87ec79168a2df1b2869d42e1252c20742723
mariamiah <mariah.kakande@gmail.com>
2020-03-20 14:33:48 +0300

    enable modal scroll on close

--- pages/spotlight/[slug]/index.tsx
+++ pages/spotlight/[slug]/index.tsx
@@ -83,6 +83,7 @@ Spotlight.getInitialProps = async (context): Promise<SpotlightProps> => {
   const { slug } = context.query;
   const scaffold = await fetchScaffoldData();
   const page = await fetchSpotlightPage(slug as string);
+
   return { scaffold, page };
 };
 
